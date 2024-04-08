ca0040a61e0bc5883dd7598b2ec24e53eac2ca7f
Nath-Assa <nathaniel.assayag@epfl.ch>
2024-04-08 15:22:06 +0200

    Update MapViewModel.kt
    Removed an unnecessary line of code from MapViewModel.kt, along with its associated imports.

--- app/src/main/java/com/github/se/eventradar/map/MapViewModel.kt
+++ app/src/main/java/com/github/se/eventradar/map/MapViewModel.kt
@@ -1,9 +1,6 @@
 package com.github.se.eventradar.map
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class MapViewModel : ViewModel() {
-  private val db = Firebase.firestore
 }
