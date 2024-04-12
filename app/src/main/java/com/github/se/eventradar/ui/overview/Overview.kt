376d22d41ef6ee3b2c542c190827bf3472bf17a3
Nath-Assa <nathaniel.assayag@epfl.ch>
2024-04-12 17:00:00 +0200

    Revert "Delete Overview.kt"
    This reverts commit d8df37609b016135f760f3d29cacb8f3c56221d2.

---
+++ app/src/main/java/com/github/se/eventradar/ui/overview/Overview.kt
@@ -0,0 +1,35 @@
+package com.github.se.eventradar.ui.overview
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.github.se.eventradar.model.OverviewUiState
+import com.github.se.eventradar.model.OverviewViewModel
+import com.github.se.eventradar.ui.navigation.NavigationActions
+
+@Composable
+fun Overview(viewModel: OverviewViewModel = viewModel(), navigationActions: NavigationActions) {
+  val uiState by viewModel.uiState.collectAsState()
+
+  // Temporarily commented out as we switch to events
+  // LaunchedEffect(key1 = uiState.toDoList) { viewModel.getToDos() }
+
+  OverviewUI({ viewModel.onSearchQueryChanged(it) }, uiState, navigationActions)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OverviewUI(
+    onSearchQueryChanged: (String) -> Unit,
+    uiState: OverviewUiState,
+    navigationActions: NavigationActions
+) {
+  var isActive by remember { mutableStateOf(false) }
+  Text(text = "HomePage")
+}
