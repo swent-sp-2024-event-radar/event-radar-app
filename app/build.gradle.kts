862bf54f2600b787d2c9214fa9567fa09cef3ece
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-04-16 23:03:12 +0200

    small changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,9 +141,7 @@ dependencies {
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
-
-    //QR CODE
-    implementation ("com.google.zxing:core:3.4.1")
+    
 }
 
 secrets {
