9e833ad255c1ba84f8a1aa7b9e0e7b1592799bb0
Christine <christine.ha@epfl.ch>
2024-04-18 13:03:50 +0200

    resolved comments and ran ktfmt formatter

--- app/src/main/java/com/github/se/eventradar/model/event/EventTicket.kt
+++ app/src/main/java/com/github/se/eventradar/model/event/EventTicket.kt
@@ -1,11 +1,7 @@
 package com.github.se.eventradar.model.event
 
-data class EventTicket(
-    val name: String,
-    val price: Double,
-    val quantity: Int
-)
+data class EventTicket(val name: String, val price: Double, val capacity: Int)
 
-fun getEventTicket(name: String, price: Double, quantity: Int): EventTicket {
-    return EventTicket(name, price, quantity)
+fun getEventTicket(name: String, price: Double, capacity: Int): EventTicket {
+  return EventTicket(name, price, capacity)
 }
