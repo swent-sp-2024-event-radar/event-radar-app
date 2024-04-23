6260790c97ec0579561aeefe01f58e4976ac8ee9
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-04-23 10:30:16 +0200

    CORRECTED TEST

--- app/src/test/java/com/github/se/eventradar/EventOverviewViewmodelUnitTest.kt
+++ app/src/test/java/com/github/se/eventradar/EventOverviewViewmodelUnitTest.kt
@@ -10,16 +10,16 @@ import java.time.LocalDateTime
 class EventOverviewViewmodelUnitTest {
     @Test
     suspend fun testEventOverviewUiInitialisation() {
-        EventDetailsViewModel().getEventData()
+        EventDetailsViewModel("pSjt3r3O8TddpWxRhGK5").getEventData()
         assert(
             EventDetailsViewModel().uiState.value
                     ==
                     EventDetailsViewModel.EventUiState(
                         eventName = "Event Test",
-                        eventPhoto = "ueuieib",
+                        eventPhoto = "hd3idd3bui",
                         start = LocalDateTime.parse("2024-12-31-T23:00:00"),
                         end = LocalDateTime.parse("2025-01-01-T05:00:00"),
-                        location = Location(38.92, 78.78, "Route Cantonale 51, 1024, Ecublens"),
+                        location = Location(38.92, 78.78, "Chemin de la colline 11, 1024, ecublens"),
                         description = "Come Party Hard like there is no tomorrow",
                         ticket = EventTicket("Standard", 0.0, 555),
                         contact = "jg@joytigoel.com",
