549c96adef8b4025b16999083825885e4aa47cea
Nath-Assa <nathaniel.assayag@epfl.ch>
2024-04-25 17:09:30 +0200

    Refactor and extend EventsOverviewViewModelTest
    - Fix failing tests with Jaeln's help
    - Add new tests to cover scenarios where:
      1. `getUpcomingEvents` is called for a user not present in the repository.
      2. `getUpcomingEvents` is called for a user with an empty attendee list.

--- app/src/test/java/com/github/se/eventradar/EventOverviewViewModelTest.kt
+++ app/src/test/java/com/github/se/eventradar/EventOverviewViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.github.se.eventradar
 
+import android.util.Log
 import com.github.se.eventradar.model.*
 import com.github.se.eventradar.model.event.Event
 import com.github.se.eventradar.model.event.EventCategory
@@ -8,19 +9,25 @@ import com.github.se.eventradar.model.repository.event.IEventRepository
 import com.github.se.eventradar.model.repository.event.MockEventRepository
 import com.github.se.eventradar.model.repository.user.IUserRepository
 import com.github.se.eventradar.model.repository.user.MockUserRepository
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.verify
 import java.time.LocalDateTime
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
 class EventsOverviewViewModelTest {
@@ -28,8 +35,21 @@ class EventsOverviewViewModelTest {
   private lateinit var viewModel: EventsOverviewViewModel
   private lateinit var eventRepository: IEventRepository
   private lateinit var userRepository: IUserRepository
-  @OptIn(DelicateCoroutinesApi::class)
-  private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+  class MainDispatcherRule(
+      private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+  ) : TestWatcher() {
+    override fun starting(description: Description) {
+      Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+      Dispatchers.resetMain()
+    }
+  }
+
+  @get:Rule val mainDispatcherRule = MainDispatcherRule()
+
   private val mockEvent =
       Event(
           eventName = "Event 1",
@@ -58,31 +78,21 @@ class EventsOverviewViewModelTest {
           eventsHostList = listOf("3"),
           profilePicUrl = "http://example.com/pic.jpg",
           qrCodeUrl = "http://example.com/qr.jpg",
-          username = "johndoe")
+          username = "john_doe")
 
   @Before
   fun setUp() {
-    Dispatchers.setMain(mainThreadSurrogate)
     eventRepository = MockEventRepository()
     userRepository = MockUserRepository()
     viewModel = EventsOverviewViewModel(eventRepository, userRepository)
   }
 
-  @After
-  fun tearDown() {
-    Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
-    mainThreadSurrogate.close()
-  }
-
   @Test
   fun testGetEventsEmpty() = runTest {
-    // Assume MockEventRepository is set to return empty list
     viewModel.getEvents()
 
-    // Allow any launched coroutines to complete
     advanceUntilIdle()
 
-    // Assertions to validate the state after fetching events
     assert(viewModel.uiState.value.eventList.allEvents.isEmpty())
     assert(viewModel.uiState.value.eventList.filteredEvents.isEmpty())
     assertNull(viewModel.uiState.value.eventList.selectedEvent)
@@ -90,30 +100,26 @@ class EventsOverviewViewModelTest {
 
   @Test
   fun testGetEventsSuccess() = runTest {
-    val event1 = mockEvent.copy(fireBaseID = "1")
-    val event2 = mockEvent.copy(fireBaseID = "2")
-    val event3 = mockEvent.copy(fireBaseID = "3")
+    val events =
+        listOf(
+            mockEvent.copy(fireBaseID = "1"),
+            mockEvent.copy(fireBaseID = "2"),
+            mockEvent.copy(fireBaseID = "3"))
 
-    eventRepository.addEvent(event1)
-    eventRepository.addEvent(event2)
-    eventRepository.addEvent(event3)
-    val events = (eventRepository.getEvents() as Resource.Success).data
+    events.forEach { event -> eventRepository.addEvent(event) }
+
     viewModel.getEvents()
     advanceUntilIdle()
     assert(viewModel.uiState.value.eventList.allEvents.isNotEmpty())
     assert(viewModel.uiState.value.eventList.allEvents.size == 3)
-    assert(
-        viewModel.uiState.value.eventList.allEvents ==
-            (eventRepository.getEvents() as Resource.Success).data)
+    assert(viewModel.uiState.value.eventList.allEvents == events)
     assert(viewModel.uiState.value.eventList.filteredEvents.size == 3)
-    assert(
-        viewModel.uiState.value.eventList.filteredEvents ==
-            (eventRepository.getEvents() as Resource.Success).data)
+    assert(viewModel.uiState.value.eventList.filteredEvents == events)
     assertNull(viewModel.uiState.value.eventList.selectedEvent)
   }
 
   @Test
-  fun testGetUpcomingEventsSuccessWithEvents() = runTest {
+  fun testGetUpcomingEventsSuccess() = runTest {
     val event1 = mockEvent.copy(fireBaseID = "1")
     val event2 = mockEvent.copy(fireBaseID = "2")
     val event3 = mockEvent.copy(fireBaseID = "3")
@@ -123,26 +129,57 @@ class EventsOverviewViewModelTest {
     eventRepository.addEvent(event3)
 
     userRepository.addUser(mockUser)
-
+    // MockUser is on the attendeeList for events with id "1" and "2"
     viewModel.getUpcomingEvents("user1")
     advanceUntilIdle()
     assert(viewModel.uiState.value.eventList.allEvents.size == 2)
-    assert(
-        viewModel.uiState.value.eventList.allEvents ==
-            (eventRepository.getEventsByIds(listOf("1", "2")) as Resource.Success).data)
+    assert(viewModel.uiState.value.eventList.allEvents == listOf(event1, event2))
     assert(viewModel.uiState.value.eventList.filteredEvents.size == 2)
-    assert(
-        viewModel.uiState.value.eventList.filteredEvents ==
-            (eventRepository.getEventsByIds(listOf("1", "2")) as Resource.Success).data)
+    assert(viewModel.uiState.value.eventList.filteredEvents == listOf(event1, event2))
     assertNull(viewModel.uiState.value.eventList.selectedEvent)
   }
 
   @Test
-  fun testGetUpcomingEventsSuccessNoEvents() = runTest {
+  fun testGetUpcomingEventsWithEventsNotInRepo() = runTest {
+    mockkStatic(Log::class)
+    every { Log.d(any(), any()) } returns 0
     (userRepository as MockUserRepository).addUser(mockUser.copy(userId = "user2"))
-
+    // MockUser is on the attendeeList for events with id "1" and "2" but these are not in the
+    // eventRepository
     viewModel.getUpcomingEvents("user2")
 
+    advanceUntilIdle()
+
+    assert(viewModel.uiState.value.eventList.allEvents.isEmpty())
+    assert(viewModel.uiState.value.eventList.filteredEvents.isEmpty())
+    assertNull(viewModel.uiState.value.eventList.selectedEvent)
+
+    verify { Log.d("EventsOverviewViewModel", "Error getting events for user2") }
+    confirmVerified()
+  }
+
+  @Test
+  fun testGetUpcomingEventsUserNotFound() = runTest {
+    mockkStatic(Log::class)
+    every { Log.d(any(), any()) } returns 0
+    val userId = "userNotFound"
+
+    viewModel.getUpcomingEvents(userId)
+    advanceUntilIdle()
+
+    assert(viewModel.uiState.value.eventList.allEvents.isEmpty())
+    assert(viewModel.uiState.value.eventList.filteredEvents.isEmpty())
+    assertNull(viewModel.uiState.value.eventList.selectedEvent)
+    verify { Log.d("EventsOverviewViewModel", "Error fetching user document") }
+    confirmVerified()
+  }
+
+  @Test
+  fun testGetUpcomingEventsEmptyAttendeeList() = runTest {
+    val userWithEmptyList =
+        mockUser.copy(userId = "userWithEmptyList", eventsAttendeeList = emptyList())
+    userRepository.addUser(userWithEmptyList)
+    viewModel.getUpcomingEvents("userWithEmptyList")
     advanceUntilIdle()
 
     assert(viewModel.uiState.value.eventList.allEvents.isEmpty())
