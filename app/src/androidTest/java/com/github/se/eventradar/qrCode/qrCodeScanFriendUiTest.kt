679b8ae1eb8742a6c0dc58cfabe05b133f79f9b5
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-04-28 17:49:35 +0200

    KMFT check

--- app/src/androidTest/java/com/github/se/eventradar/qrCode/qrCodeScanFriendUiTest.kt
+++ app/src/androidTest/java/com/github/se/eventradar/qrCode/qrCodeScanFriendUiTest.kt
@@ -1,14 +1,11 @@
 package com.github.se.eventradar.qrCode
 
-import android.os.Build
+import android.Manifest
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject
-import androidx.test.uiautomator.UiObjectNotFoundException
-import androidx.test.uiautomator.UiSelector
 import com.github.se.eventradar.screens.QrCodeScanFriendUiScreen
 import com.github.se.eventradar.ui.navigation.NavigationActions
 import com.github.se.eventradar.ui.qrCode.QrCodeScreen
@@ -17,7 +14,6 @@ import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.mockk.impl.annotations.RelaxedMockK
-import android.Manifest
 import io.mockk.junit4.MockKRule
 import org.junit.Before
 import org.junit.Rule
@@ -27,18 +23,15 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class QrCodeScanFriendUiTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
-  @get:Rule
-  val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createComposeRule()
 
   @get:Rule
   val mRuntimePermissionRule: GrantPermissionRule =
-    GrantPermissionRule.grant(Manifest.permission.CAMERA)
+      GrantPermissionRule.grant(Manifest.permission.CAMERA)
 
-  @get:Rule
-  val mockkRule = MockKRule(this)
+  @get:Rule val mockkRule = MockKRule(this)
 
-  @RelaxedMockK
-  lateinit var mockNavActions: NavigationActions
+  @RelaxedMockK lateinit var mockNavActions: NavigationActions
 
   private lateinit var mDevice: UiDevice
 
@@ -53,7 +46,7 @@ class QrCodeScanFriendUiTest : TestCase(kaspressoBuilder = Kaspresso.Builder.wit
   fun displaysAllComponentsCorrectly_CameraPermittedAlways(): Unit = run {
     onComposeScreen<QrCodeScanFriendUiScreen>(composeTestRule) {
       scanQrTab.performClick()
-//      allowPermissionsWhileUsingApp()
+      //      allowPermissionsWhileUsingApp()
 
       logo.assertIsDisplayed()
 
@@ -110,7 +103,8 @@ class QrCodeScanFriendUiTest : TestCase(kaspressoBuilder = Kaspresso.Builder.wit
 //
 //  private fun allowPermissionsWhileUsingApp() {
 //    if (Build.VERSION.SDK_INT >= 23) {
-//      val allowPermissions: UiObject = mDevice.findObject(UiSelector().text("While using the app"))
+//      val allowPermissions: UiObject = mDevice.findObject(UiSelector().text("While using the
+// app"))
 //      if (allowPermissions.exists()) {
 //        try {
 //          allowPermissions.click()
@@ -149,7 +143,7 @@ class QrCodeScanFriendUiTest : TestCase(kaspressoBuilder = Kaspresso.Builder.wit
 //      }
 //    }
 //  }
-//}
+// }
 
 //                logo { assertIsDisplayed() }
 //                tabs { assertIsDisplayed() }
