b7390fa495eb33966eab8b0ab68f9d96b3c72346
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-04-30 01:34:23 +0200

    Changed contact to mainOrganiser

--- app/src/main/java/com/github/se/eventradar/model/event/Event.kt
+++ app/src/main/java/com/github/se/eventradar/model/event/Event.kt
@@ -11,62 +11,62 @@ data class Event(
     var location: Location,
     var description: String,
     var ticket: EventTicket,
-    var contact: String,
+    var mainOrganiser: String,
     var organiserList: Set<String>,
     var attendeeList: Set<String>,
     var category: EventCategory,
     var fireBaseID: String
 ) {
-  constructor(
-      map: Map<String, Any>,
-      id: String
-  ) : this(
-      eventName = map["name"] as String,
-      eventPhoto = map["photo_url"] as String,
-      start = LocalDateTime.parse(map["start"] as String),
-      end = LocalDateTime.parse(map["end"] as String),
-      location =
-          Location(
-              latitude = map["location_lat"] as Double,
-              longitude = map["location_lng"] as Double,
-              address = map["location_name"] as String),
-      description = map["description"] as String,
-      ticket =
-          EventTicket(
-              name = map["ticket_name"] as String,
-              price = map["ticket_price"] as Double,
-              capacity = map["ticket_quantity"] as Int),
-      contact = map["contact"] as String,
-      organiserList = getSetOfStrings(map["organisers_list"]),
-      attendeeList = getSetOfStrings(map["attendees_list"]),
-      category = EventCategory.valueOf(map["category"] as String),
-      fireBaseID = id)
+    constructor(
+        map: Map<String, Any>,
+        id: String
+    ) : this(
+        eventName = map["name"] as String,
+        eventPhoto = map["photo_url"] as String,
+        start = LocalDateTime.parse(map["start"] as String),
+        end = LocalDateTime.parse(map["end"] as String),
+        location =
+        Location(
+            latitude = map["location_lat"] as Double,
+            longitude = map["location_lng"] as Double,
+            address = map["location_name"] as String),
+        description = map["description"] as String,
+        ticket =
+        EventTicket(
+            name = map["ticket_name"] as String,
+            price = (map["ticket_price"] as Long).toDouble(),
+            capacity = (map["ticket_quantity"] as Long).toInt()),
+        mainOrganiser = map["main_organiser"] as String,
+        organiserList = getSetOfStrings(map["organisers_list"]),
+        attendeeList = getSetOfStrings(map["attendees_list"]),
+        category = EventCategory.valueOf(map["category"] as String),
+        fireBaseID = id)
 
-  fun toMap(): HashMap<String, Any> {
-    val map = HashMap<String, Any>()
-    map["name"] = eventName
-    map["photo_url"] = eventPhoto
-    map["start"] = start.toString()
-    map["end"] = end.toString()
-    map["location_name"] = location.address
-    map["location_lat"] = location.latitude
-    map["location_lng"] = location.longitude
-    map["description"] = description
-    map["ticket_name"] = ticket.name
-    map["ticket_price"] = ticket.price
-    map["ticket_quantity"] = ticket.capacity
-    map["contact"] = contact
-    map["organisers_list"] = organiserList.toList()
-    map["attendees_list"] = attendeeList.toList()
-    map["category"] = category.name
-    return map
-  }
+    fun toMap(): HashMap<String, Any> {
+        val map = HashMap<String, Any>()
+        map["name"] = eventName
+        map["photo_url"] = eventPhoto
+        map["start"] = start.toString()
+        map["end"] = end.toString()
+        map["location_name"] = location.address
+        map["location_lat"] = location.latitude
+        map["location_lng"] = location.longitude
+        map["description"] = description
+        map["ticket_name"] = ticket.name
+        map["ticket_price"] = ticket.price
+        map["ticket_quantity"] = ticket.capacity
+        map["main_organiser"] = mainOrganiser
+        map["organisers_list"] = organiserList.toList()
+        map["attendees_list"] = attendeeList.toList()
+        map["category"] = category.name
+        return map
+    }
 }
 
 private fun getSetOfStrings(data: Any?): Set<String> {
-  return when (data) {
-    is List<*> -> data.filterIsInstance<String>().toSet()
-    is String -> setOf(data)
-    else -> emptySet()
-  }
+    return when (data) {
+        is List<*> -> data.filterIsInstance<String>().toSet()
+        is String -> setOf(data)
+        else -> emptySet()
+    }
 }
