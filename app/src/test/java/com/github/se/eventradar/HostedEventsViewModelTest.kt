01e2d84f3f48c179ba47f429047826026e91fb19
Jalen Geason <64320145+jgeaso1266@users.noreply.github.com>
2024-04-29 12:18:27 +0200

    Remove `confirmVerified` call that was breaking tests

--- app/src/test/java/com/github/se/eventradar/HostedEventsViewModelTest.kt
+++ app/src/test/java/com/github/se/eventradar/HostedEventsViewModelTest.kt
@@ -11,7 +11,6 @@ import com.github.se.eventradar.model.repository.event.MockEventRepository
 import com.github.se.eventradar.model.repository.user.IUserRepository
 import com.github.se.eventradar.model.repository.user.MockUserRepository
 import com.github.se.eventradar.viewmodel.HostedEventsViewModel
-import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.verify
@@ -140,7 +139,6 @@ class HostedEventsViewModelTest {
       Log.d(
           "HostedEventsViewModel", "Error getting hosted events for ${userWithHostedEvent.userId}")
     }
-    confirmVerified()
   }
 
   @Test
@@ -153,6 +151,5 @@ class HostedEventsViewModelTest {
     assert(viewModel.uiState.value.eventList.filteredEvents.isEmpty())
     Assert.assertNull(viewModel.uiState.value.eventList.selectedEvent)
     verify { Log.d("HostedEventsViewModel", "Error fetching user document") }
-    confirmVerified()
   }
 }
