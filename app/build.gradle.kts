8eec384220cfefdb4fbe03a9da3f82adc95564bb
Jalen Geason <64320145+jgeaso1266@users.noreply.github.com>
2024-05-01 23:54:48 +0200

    Fix signing key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = null
         }
         debug {
             enableUnitTestCoverage = true
