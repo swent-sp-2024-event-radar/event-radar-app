3729876b33a91d928904ff9adc2f1b2a5ad84c9b
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-05-02 21:22:45 +0200

    inject

--- app/src/main/java/com/github/se/eventradar/ui/qrCode/QrCodeScanner.kt
+++ app/src/main/java/com/github/se/eventradar/ui/qrCode/QrCodeScanner.kt
@@ -29,55 +29,58 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.github.se.eventradar.viewmodel.qrCode.QrCodeAnalyser
 
-@Composable
-fun QrCodeScanner(analyser: QrCodeAnalyser) {
+  @Composable
+  fun QrCodeScanner(analyser: QrCodeAnalyser) {
 
-  val context = LocalContext.current
-  val lifeCycleOwner = LocalLifecycleOwner.current
-  val cameraFutureProvider = remember { ProcessCameraProvider.getInstance(context) }
+    val context = LocalContext.current
+    val lifeCycleOwner = LocalLifecycleOwner.current
+    val cameraFutureProvider = remember { ProcessCameraProvider.getInstance(context) }
 
-  var hasCameraPermission by remember {
-    mutableStateOf(
-        ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) ==
-            PackageManager.PERMISSION_GRANTED)
-  }
+    var hasCameraPermission by remember {
+      mutableStateOf(
+          ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) ==
+              PackageManager.PERMISSION_GRANTED)
+    }
 
-  val launcher =
-      rememberLauncherForActivityResult(
-          contract = ActivityResultContracts.RequestPermission(),
-          onResult = { granted -> hasCameraPermission = granted })
-  // only ask permission once
-  LaunchedEffect(key1 = true) { launcher.launch(android.Manifest.permission.CAMERA) }
-  Column(modifier = Modifier.fillMaxSize()) {
-    if (hasCameraPermission) {
-      Spacer(modifier = Modifier.height(80.dp))
-      AndroidView( // PreviewView !E for Composable hence need ot create AndroidView
-          factory = { context ->
-            val previewView = PreviewView(context)
-            val preview = Preview.Builder().build()
-            val selector =
-                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-            preview.setSurfaceProvider(previewView.surfaceProvider)
-            val imageAnalysis =
-                ImageAnalysis.Builder()
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .build()
-            imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), analyser)
+    val launcher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { granted -> hasCameraPermission = granted })
+    // only ask permission once
+    LaunchedEffect(key1 = true) { launcher.launch(android.Manifest.permission.CAMERA) }
+    Column(modifier = Modifier.fillMaxSize()) {
+      if (hasCameraPermission) {
+        Spacer(modifier = Modifier.height(80.dp))
+        AndroidView( // PreviewView !E for Composable hence need ot create AndroidView
+            factory = { context ->
+              val previewView = PreviewView(context)
+              val preview = Preview.Builder().build()
+              val selector =
+                  CameraSelector.Builder()
+                      .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                      .build()
+              preview.setSurfaceProvider(previewView.surfaceProvider)
+              val imageAnalysis =
+                  ImageAnalysis.Builder()
+                      .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                      .build()
+              imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), analyser)
 
-            try {
-              cameraFutureProvider
-                  .get()
-                  .bindToLifecycle(
-                      lifeCycleOwner, // only launched during current compose lifecycle
-                      selector,
-                      preview,
-                      imageAnalysis)
-            } catch (e: Exception) {
-              Log.e("QrCodeCameraFail", "Use case binding failed", e)
-            }
-            previewView
-          },
-          modifier = Modifier.weight(1.5f).aspectRatio(1f).padding(horizontal = 32.dp))
+              try {
+                cameraFutureProvider
+                    .get()
+                    .bindToLifecycle(
+                        lifeCycleOwner, // only launched during current compose lifecycle
+                        selector,
+                        preview,
+                        imageAnalysis)
+              } catch (e: Exception) {
+                Log.e("QrCodeCameraFail", "Use case binding failed", e)
+              }
+              previewView
+            },
+            modifier = Modifier.weight(1.5f).aspectRatio(1f).padding(horizontal = 32.dp))
+      }
     }
   }
-}
+
