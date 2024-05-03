be46dc335cfbb9daf733059fb2ff1a2a01a0e37f
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-05-03 19:14:17 +0200

    step 3 Test Vm Work

--- app/src/main/java/com/github/se/eventradar/viewmodel/qrCode/QrCodeAnalyser.kt
+++ app/src/main/java/com/github/se/eventradar/viewmodel/qrCode/QrCodeAnalyser.kt
@@ -1,3 +1,5 @@
+package com.github.se.eventradar.viewmodel.qrCode
+
 import android.graphics.ImageFormat
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
@@ -13,50 +15,54 @@ import javax.inject.Inject
 
 class QrCodeAnalyser @Inject constructor() : ImageAnalysis.Analyzer {
 
-    // list of supported Image Formats
-    private val supportedImageFormats =
-        listOf(ImageFormat.YUV_420_888, ImageFormat.YUV_422_888, ImageFormat.YUV_444_888)
+  // list of supported Image Formats
+  private val supportedImageFormats =
+      listOf(ImageFormat.YUV_420_888, ImageFormat.YUV_422_888, ImageFormat.YUV_444_888)
 
-    var onDecoded: ((String?) -> Unit)? = null //
+  var onDecoded: ((String?) -> Unit)? = null //
 
-    override fun analyze(image: ImageProxy) {
+  override fun analyze(image: ImageProxy) {
 
-        // only want to scan if it is a QR Code
-        if (image.format in supportedImageFormats) {
-            val bytes = image.planes.first().buffer.toByteArray()
+    // only want to scan if it is a QR Code
+    if (image.format in supportedImageFormats) {
+      val bytes = image.planes.first().buffer.toByteArray()
 
-            // parameters to scan
-            val source =
-                PlanarYUVLuminanceSource(
-                    bytes, image.width, image.height, 0, 0, image.width, image.height, false)
+      // parameters to scan
+      val source =
+          PlanarYUVLuminanceSource(
+              bytes, image.width, image.height, 0, 0, image.width, image.height, false)
 
-            val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-            try {
+      val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
+      try {
 
-                // result is the info encoded in to QR Code (String of userId in our case)
-                val result =
-                    MultiFormatReader()
-                        .apply {
-                            setHints(
-                                mapOf(DecodeHintType.POSSIBLE_FORMATS to arrayListOf(BarcodeFormat.QR_CODE)))
-                        }
-                        .decode(binaryBitmap)
-                // Log the decoded string
-                Log.d("QrCodeAnalyser", "Decoded string: ${result.text}")
-                // if onDecoded is null (has not been initialised b Viewmodel) will simply return Null
-                onDecoded?.invoke(result.toString())
-            } catch (e: Exception) {
+        // result is the info encoded in to QR Code (String of userId in our case)
+        val result =
+            MultiFormatReader()
+                .apply {
+                  setHints(
+                      mapOf(DecodeHintType.POSSIBLE_FORMATS to arrayListOf(BarcodeFormat.QR_CODE)))
+                }
+                .decode(binaryBitmap)
+        // Log the decoded string
+        Log.d(
+            "com.github.se.eventradar.viewmodel.qrCode.QrCodeAnalyser",
+            "Decoded string: ${result.text}")
+        // if onDecoded is null (has not been initialised b Viewmodel) will simply return Null
+        onDecoded?.invoke(result.toString())
+      } catch (e: Exception) {
 
-                Log.d("QrCodeAnalyser", "Error decoding QR Code: ${e.message}")
-            } finally { // close image once scanning process done
-                image.close()
-            }
-        }
+        Log.d(
+            "com.github.se.eventradar.viewmodel.qrCode.QrCodeAnalyser",
+            "Error decoding QR Code: ${e.message}")
+      } finally { // close image once scanning process done
+        image.close()
+      }
     }
+  }
 
-    // method to return all bytes in a ByteArray
-    private fun ByteBuffer.toByteArray(): ByteArray {
-        rewind()
-        return ByteArray(remaining()).also { get(it) }
-    }
+  // method to return all bytes in a ByteArray
+  private fun ByteBuffer.toByteArray(): ByteArray {
+    rewind()
+    return ByteArray(remaining()).also { get(it) }
+  }
 }
