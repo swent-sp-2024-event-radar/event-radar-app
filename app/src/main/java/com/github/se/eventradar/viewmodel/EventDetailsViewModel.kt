e12ca49f908124d31d3c2df52e8baf0895e89d61
Viccha18 <93199205+Viccha18@users.noreply.github.com>
2024-05-03 14:02:51 +0200

    added Nikhen's function "getTickets"

--- app/src/main/java/com/github/se/eventradar/viewmodel/EventDetailsViewModel.kt
+++ app/src/main/java/com/github/se/eventradar/viewmodel/EventDetailsViewModel.kt
@@ -62,6 +62,47 @@ constructor(
   fun isTicketFree(): Boolean {
     return !(_uiState.value.ticket.price > 0.0)
   }
+
+  fun getTickets() : Unit{ //update ticket, while buying ticket give user 5 minutes to buy?
+    viewModelScope.launch {
+      when (val response = eventRepository.getEvent(eventId)) {
+        is Resource.Success -> {
+          _uiState.update {
+            it.copy(ticket = response.data!!.ticket)
+          }
+        }
+        is Resource.Failure ->
+          Log.d("EventDetailsViewModel", "Error getting ticket for event: ${response.throwable.message}")
+      }
+    }
+  }
+
+  /*TODO actually, I had to recreate a view model when switching to the
+   * buy ticket screen so that function is not needed anymore...
+   */
+  /*fun buyTicketForEvent() : Boolean{ //update the user attendee list, update the eventRepo ticket count
+    viewModelScope.launch{
+      if (!isTicketFree()){
+
+      }
+      //event id, new event values.
+      val newState = _uiState.value.copy(ticket = _uiState.value.ticket.copy(capacity = _uiState.value.ticket.capacity-1))
+      val newEvent = Event(newState, eventId) //eventUiState to Event?
+
+      // TODO would need an atomic update of the database
+      when (val response = eventRepository.updateEvent(newEvent)) {
+      is Resource.Success -> {
+      _uiState.update {
+        it.copy(ticket = response.data!!.ticket)
+      }
+    }
+      is Resource.Failure ->
+      Log.d("EventDetailsViewModel", "Error buying ticket for event: ${response.throwable.message}")
+    }
+    }
+    return true
+  }*/
+
 }
 
 data class EventUiState(
