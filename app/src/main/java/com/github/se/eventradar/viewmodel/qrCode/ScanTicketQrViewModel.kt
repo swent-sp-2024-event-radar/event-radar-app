48da94d825d8edbc1299e6da7c2e57ce09750883
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-05-09 19:49:46 +0200

    kmft

--- app/src/main/java/com/github/se/eventradar/viewmodel/qrCode/ScanTicketQrViewModel.kt
+++ app/src/main/java/com/github/se/eventradar/viewmodel/qrCode/ScanTicketQrViewModel.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 class ScanTicketQrViewModel
