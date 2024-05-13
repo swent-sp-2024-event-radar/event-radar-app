a78977259791511e89487e262746b1a5c1146c55
Jalen Geason <64320145+jgeaso1266@users.noreply.github.com>
2024-05-13 20:36:42 +0200

    Refactor search bar to be its own composable

--- app/src/main/java/com/github/se/eventradar/ui/component/ReusableComponents.kt
+++ app/src/main/java/com/github/se/eventradar/ui/component/ReusableComponents.kt
@@ -146,29 +146,13 @@ fun EventCard(event: Event, onCardClick: (String) -> Unit) {
 @Composable
 fun SearchBarAndFilter(
     onSearchQueryChanged: (String) -> Unit,
-    uiState: EventsOverviewUiState,
+    searchQuery: String,
     onSearchActiveChanged: (Boolean) -> Unit,
     onFilterDialogOpen: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
   Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
-    // Search bar
-    TextField(
-        value = uiState.searchQuery,
-        onValueChange = {
-          onSearchQueryChanged(it)
-          if (it == "") onSearchActiveChanged(false) else onSearchActiveChanged(true)
-        },
-        modifier = Modifier.weight(1f).testTag("searchBar"),
-        maxLines = 1,
-        shape = RoundedCornerShape(32.dp),
-        colors =
-            TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent),
-        placeholder = { Text(stringResource(id = R.string.home_search_placeholder)) },
-        trailingIcon = { Icon(Icons.Default.Search, contentDescription = null) })
+    SearchBarField(searchQuery, onSearchQueryChanged, onSearchActiveChanged, Modifier.weight(1f))
 
     // Filter button
     Button(
@@ -177,6 +161,32 @@ fun SearchBarAndFilter(
           Text(stringResource(id = R.string.filter))
         }
   }
+}
+
+@Composable
+fun SearchBarField(
+    searchQuery: String,
+    onSearchQueryChanged: (String) -> Unit,
+    onSearchActiveChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+  // Search bar
+  TextField(
+      value = searchQuery,
+      onValueChange = {
+        onSearchQueryChanged(it)
+        if (it == "") onSearchActiveChanged(false) else onSearchActiveChanged(true)
+      },
+      modifier = modifier.testTag("searchBar"),
+      maxLines = 1,
+      shape = RoundedCornerShape(32.dp),
+      colors =
+          TextFieldDefaults.colors(
+              focusedIndicatorColor = Color.Transparent,
+              unfocusedIndicatorColor = Color.Transparent,
+              disabledIndicatorColor = Color.Transparent),
+      placeholder = { Text(stringResource(id = R.string.home_search_placeholder)) },
+      trailingIcon = { Icon(Icons.Default.Search, contentDescription = null) })
 }
 
 @Composable
