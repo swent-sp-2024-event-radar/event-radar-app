024b803d0d0f0bd19be9717c45902b14c451e024
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-05-14 15:36:01 +0200

    Update ScanTicketQrViewModelTest.kt

--- app/src/test/java/com/github/se/eventradar/ScanTicketQrViewModelTest.kt
+++ app/src/test/java/com/github/se/eventradar/ScanTicketQrViewModelTest.kt
@@ -51,8 +51,7 @@ class ScanFriendTicketViewModelTest {
             organiserList = mutableListOf("Test Organiser"),
             attendeeList = mutableListOf("user1", "user2", "user3"),
             category = EventCategory.COMMUNITY,
-            fireBaseID = "1"
-        )
+            fireBaseID = "1")
 
     private val mockUser1 =
         User(
@@ -68,8 +67,7 @@ class ScanFriendTicketViewModelTest {
             friendsList = mutableListOf(),
             profilePicUrl = "http://example.com/Profile_Pictures/1",
             qrCodeUrl = "http://example.com/QR_Codes/1",
-            username = "johndoe"
-        )
+            username = "johndoe")
 
     private val mockUser2 =
         User(
@@ -85,8 +83,7 @@ class ScanFriendTicketViewModelTest {
             friendsList = mutableListOf(),
             profilePicUrl = "http://example.com/Profile_Pictures/1",
             qrCodeUrl = "http://example.com/QR_Codes/1",
-            username = "johndoe"
-        )
+            username = "johndoe")
 
     class MainDispatcherRule(
         private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
@@ -100,8 +97,7 @@ class ScanFriendTicketViewModelTest {
         }
     }
 
-    @get:Rule
-    val mainDispatcherRule = MainDispatcherRule()
+    @get:Rule val mainDispatcherRule = MainDispatcherRule()
 
     @Before
     fun setUp() {
@@ -126,61 +122,52 @@ class ScanFriendTicketViewModelTest {
         eventRepository.addEvent(mockEvent1)
         qrCodeAnalyser.onDecoded?.invoke(null)
         TestCase.assertEquals(
-            ScanTicketQrViewModel.Action.AnalyserError, viewModel.uiState.value.action
-        )
+            ScanTicketQrViewModel.Action.AnalyserError, viewModel.uiState.value.action)
+    }
 
-        @Test
-        fun testInvokedAndUpdatedOnce() = runTest {
-            userRepository.addUser(mockUser1)
-            eventRepository.addEvent(mockEvent1)
-            qrCodeAnalyser.onDecoded?.invoke("user1")
-            when (val event1 = eventRepository.getEvent("1")) {
-                is Resource.Success -> {
-                    TestCase.assertEquals(
-                        mutableListOf("user2", "user3"),
-                        event1.data!!.attendeeList
-                    )
-                }
-
-                else -> {
-                    assert(false)
-                    println("User 2 not found or could not be fetched")
-                }
+    @Test
+    fun testInvokedAndUpdatedOnce() = runTest {
+        userRepository.addUser(mockUser1)
+        eventRepository.addEvent(mockEvent1)
+        qrCodeAnalyser.onDecoded?.invoke("user1")
+        when (val event1 = eventRepository.getEvent("1")) {
+            is Resource.Success -> {
+                TestCase.assertEquals(mutableListOf("user2", "user3"), event1.data!!.attendeeList)
             }
-            when (val user1 = userRepository.getUser("user1")) {
-                is Resource.Success -> {
-                    TestCase.assertEquals(mutableListOf("2", "3"), user1.data!!.eventsAttendeeList)
-                }
-
-                else -> {
-                    assert(false)
-                    println("User 1 not found or could not be fetched")
-                }
+            else -> {
+                assert(false)
+                println("User 2 not found or could not be fetched")
             }
-            TestCase.assertEquals(
-                ScanTicketQrViewModel.Action.ApproveEntry,
-                viewModel.uiState.value.action
-            )
         }
+        when (val user1 = userRepository.getUser("user1")) {
+            is Resource.Success -> {
+                TestCase.assertEquals(mutableListOf("2", "3"), user1.data!!.eventsAttendeeList)
+            }
+            else -> {
+                assert(false)
+                println("User 1 not found or could not be fetched")
+            }
+        }
+        TestCase.assertEquals(ScanTicketQrViewModel.Action.ApproveEntry, viewModel.uiState.value.action)
+    }
 
-        @Test
-        fun testInvokedAndFetchError() = runTest {
-            userRepository.addUser(mockUser1)
-            eventRepository.addEvent(mockEvent1)
-            qrCodeAnalyser.onDecoded?.invoke("user5")
-            advanceUntilIdle()
-            TestCase.assertEquals(
-                ScanTicketQrViewModel.Action.FirebaseFetchError, viewModel.uiState.value.action
-            )
-        }
+    @Test
+    fun testInvokedAndFetchError() = runTest {
+        userRepository.addUser(mockUser1)
+        eventRepository.addEvent(mockEvent1)
+        qrCodeAnalyser.onDecoded?.invoke("user5")
+        advanceUntilIdle()
+        TestCase.assertEquals(
+            ScanTicketQrViewModel.Action.FirebaseFetchError, viewModel.uiState.value.action)
+    }
 
-        @Test
-        fun testInvokedAndDenied() = runTest {
-            userRepository.addUser(mockUser2)
-            eventRepository.addEvent(mockEvent1)
-            qrCodeAnalyser.onDecoded?.invoke("user2")
-            advanceUntilIdle()
-            assertEquals(ScanTicketQrViewModel.Action.DenyEntry, viewModel.uiState.value.action)
-        }
+    @Test
+    fun testInvokedAndDenied() = runTest {
+        userRepository.addUser(mockUser2)
+        eventRepository.addEvent(mockEvent1)
+        qrCodeAnalyser.onDecoded?.invoke("user2")
+        advanceUntilIdle()
+        assertEquals(ScanTicketQrViewModel.Action.DenyEntry, viewModel.uiState.value.action)
     }
 }
+
