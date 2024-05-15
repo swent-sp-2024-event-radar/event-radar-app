36f740ac005e34c14846be3d0d612f9d2407959a
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-05-15 17:57:17 +0200

    Added in missing line in toMap() Event.kt

--- app/src/main/java/com/github/se/eventradar/model/event/Event.kt
+++ app/src/main/java/com/github/se/eventradar/model/event/Event.kt
@@ -56,7 +56,8 @@ data class Event(
     map["description"] = description
     map["ticket_name"] = ticket.name
     map["ticket_price"] = ticket.price
-    map["ticket_quantity"] = ticket.capacity
+    map["ticket_capacity"] = ticket.capacity
+    map["ticket_purchases"] = ticket.purchases
     map["main_organiser"] = mainOrganiser
     map["organisers_list"] = organiserList.toList()
     map["attendees_list"] = attendeeList.toList()
