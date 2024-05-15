a8e7a2ef3c181f4903ddbdaf14dfdd60d31f7ec9
Viccha18 <93199205+Viccha18@users.noreply.github.com>
2024-05-15 16:18:31 +0200

    hilt injection in test + test for EventDetailsViewModel instantiation

--- app/src/androidTest/java/com/github/se/eventradar/di/MockDatabaseModules.kt
+++ app/src/androidTest/java/com/github/se/eventradar/di/MockDatabaseModules.kt
@@ -13,7 +13,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.components.SingletonComponent
 import dagger.hilt.testing.TestInstallIn
-import javax.inject.Singleton
 
 @Module
 @TestInstallIn(
--- app/src/androidTest/java/com/github/se/eventradar/event/UITest.kt
+++ app/src/androidTest/java/com/github/se/eventradar/event/UITest.kt
@@ -1,133 +1,94 @@
 package com.github.se.eventradar.event
 
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.se.eventradar.model.Location
 import com.github.se.eventradar.model.event.Event
 import com.github.se.eventradar.model.event.EventCategory
 import com.github.se.eventradar.model.event.EventDetailsViewModel
 import com.github.se.eventradar.model.event.EventTicket
-import com.github.se.eventradar.model.event.EventUiState
 import com.github.se.eventradar.model.repository.event.IEventRepository
-import com.github.se.eventradar.model.repository.event.MockEventRepository
-import com.github.se.eventradar.screens.EventDetailsScreen
-import com.github.se.eventradar.ui.event.EventDetails
+import com.github.se.eventradar.ui.MainActivity
 import com.github.se.eventradar.ui.navigation.NavigationActions
-import com.github.se.eventradar.ui.navigation.Route
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.github.kakaocup.compose.node.element.ComposeScreen
-import io.mockk.confirmVerified
-import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
-import io.mockk.verify
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import java.time.LocalDateTime
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
+import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestWatcher
-import org.junit.runner.Description
-import org.junit.runner.RunWith
-import javax.inject.Inject
 
 @HiltAndroidTest
 @OptIn(ExperimentalCoroutinesApi::class)
 class UITest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
-  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule var hiltRule = HiltAndroidRule(this)
+
+  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
   // This rule automatic initializes lateinit properties with @MockK, @RelaxedMockK, etc.
   @get:Rule val mockkRule = MockKRule(this)
 
-  @get:Rule
-  var hiltRule = HiltAndroidRule(this)
-
-
-  class MainDispatcherRule(
-    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
-  ) : TestWatcher() {
-    override fun starting(description: Description) {
-      Dispatchers.setMain(testDispatcher)
-    }
-
-
-    override fun finished(description: Description) {
-      Dispatchers.resetMain()
-    }
-  }
-
-  @get:Rule val mainDispatcherRule = MainDispatcherRule()
-
-
-  @Inject
-  lateinit var eventRepository: IEventRepository
-
-  private lateinit var viewModel: EventDetailsViewModel
-
-
-  private val factory =
-    object : EventDetailsViewModel.Factory {
-      override fun create(eventId: String): EventDetailsViewModel {
-        return EventDetailsViewModel(eventRepository, eventId)
-      }
-    }
-
   // Relaxed mocks methods have a default implementation returning values
   @RelaxedMockK lateinit var mockNavActions: NavigationActions
 
-
   private val mockEvent =
-    Event(
-      eventName = "Event 1",
-      eventPhoto = "",
-      start = LocalDateTime.now(),
-      end = LocalDateTime.now(),
-      location = Location(0.0, 0.0, "Test Location"),
-      description = "Test Description",
-      ticket = EventTicket("Test Ticket", 0.0, 1),
-      mainOrganiser = "1",
-      organiserList = mutableListOf("Test Organiser"),
-      attendeeList = mutableListOf("Test Attendee"),
-      category = EventCategory.COMMUNITY,
-      fireBaseID = "1")
+      Event(
+          eventName = "Event 1",
+          eventPhoto = "",
+          start = LocalDateTime.now(),
+          end = LocalDateTime.now(),
+          location = Location(0.0, 0.0, "Test Location"),
+          description = "Test Description",
+          ticket = EventTicket("Test Ticket", 0.0, 1),
+          mainOrganiser = "1",
+          organiserList = mutableListOf("Test Organiser"),
+          attendeeList = mutableListOf("Test Attendee"),
+          category = EventCategory.COMMUNITY,
+          fireBaseID = "1")
 
+  @Inject lateinit var eventRepository: IEventRepository
+
+  private lateinit var viewModel: EventDetailsViewModel
+
+  private val factory =
+      object : EventDetailsViewModel.Factory {
+        override fun create(eventId: String): EventDetailsViewModel {
+          return EventDetailsViewModel(eventRepository, eventId)
+        }
+      }
 
   @Before
   fun testSetup() {
-
     hiltRule.inject()
-
-    viewModel = factory.create(eventId = mockEvent.fireBaseID)
-
-    composeTestRule.setContent {
-      EventDetails(viewModel,
-      navigationActions = mockNavActions) }
   }
 
   @Test
-  fun screenDisplaysNavigationElementsCorrectly() = runTest {
-
-    (eventRepository as MockEventRepository).addEvent(mockEvent)
-    viewModel.getEventData()
-
-    ComposeScreen.onComposeScreen<EventDetailsScreen>(composeTestRule) {
-      assert(viewModel.uiState.value.eventName == mockEvent.eventName)
+  fun eventDetailsViewModelCorrectlyInstanced() = runTest {
+    composeTestRule.clearAndSetContent {
+      viewModel = EventDetailsViewModel.create(eventId = mockEvent.fireBaseID)
+      assert(mockEvent.fireBaseID == viewModel.eventId)
     }
   }
+}
 
+// Solution from here :
+// https://stackoverflow.com/questions/73191692/test-errormyactivity-has-already-set-content-if-you-have-populated-the-activit
+
+fun AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>.clearAndSetContent(
+    content: @Composable () -> Unit
+) {
+  (this.activity.findViewById<ViewGroup>(android.R.id.content)?.getChildAt(0) as? ComposeView)
+      ?.setContent(content) ?: this.setContent(content)
 }
