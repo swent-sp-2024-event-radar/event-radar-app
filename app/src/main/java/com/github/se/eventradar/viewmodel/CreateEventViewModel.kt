ab9f418c11a1464efa1f06b65f097ee21568f237
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-05-16 11:01:24 +0200

    Added a succesful unit test

--- app/src/main/java/com/github/se/eventradar/viewmodel/CreateEventViewModel.kt
+++ app/src/main/java/com/github/se/eventradar/viewmodel/CreateEventViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.se.eventradar.model.Location
 import com.github.se.eventradar.model.Resource
+import com.github.se.eventradar.model.User
 import com.github.se.eventradar.model.event.Event
 import com.github.se.eventradar.model.event.EventCategory
 import com.github.se.eventradar.model.event.EventList
@@ -15,7 +16,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -34,7 +37,28 @@ constructor(
                 when (userIdResource) {
                     is Resource.Success -> {
                         val uid = userIdResource.data
-                        addUserEvent(uid)
+                        val eventHashMap =
+                            hashMapOf(
+                                //do the types have to be correct?
+                                "name" to state.value.eventName,
+                                "photo_url" to state.value.eventPhotoUrl,
+                                "start" to state.value.startDate +"T"+ state.value.startTime,
+                                "end" to state.value.endDate +"T"+ state.value.endTime,
+                                "location_lat" to state.value.location, //convert location into lat and long
+                                "location_lng" to state.value.location,
+                                "location_name" to state.value.location,
+                                "description" to state.value.eventDescription,
+                                "ticket_name" to state.value.ticketName,
+                                "ticket_price" to state.value.ticketPrice,
+                                "ticket_capacity" to state.value.ticketCapacity,
+                                "ticket_purchases" to 0,
+                                "main_organiser" to uid,
+                                "organisers_list" to state.value.organiserList,
+                                "attendees_list" to mutableListOf<String>(),
+                                "category" to state.value.eventCategory,
+                            )
+                        val newEvent = Event(eventHashMap)
+                        addUserEvent(uid, newEvent)
                     }
                     is Resource.Failure -> {
                         Log.d("CreateEventViewModel", "User not logged in or error fetching user ID")
@@ -46,34 +70,14 @@ constructor(
         }
     }
 
-    //fun checkEventFields - is it null or not?
-
-    private suspend fun addUserEvent(uid: String, state: MutableStateFlow<CreateEventUiState> = _uiState) {
+    private suspend fun addUserEvent(uid: String, newEvent: Event, state: MutableStateFlow<CreateEventUiState> = _uiState) {
         //update the user event list
-        //update the event list
+        //add the event.
         viewModelScope.launch {
-            val eventHashMap =
-                hashMapOf(
-                    "name" to state.value.eventName,
-                    "photo_url" to state.value.eventPhoto,
-                    "start" to state.value.start,
-                    "end" to state.value.end,
-                    "location_lat" to state.value.location.latitude,
-                    "location_lng" to state.value.location.longitude,
-                    "location_name" to state.value.location.address,
-                    "description" to state.value.description,
-                    "ticket_name" to state.value.ticket.name,
-                    "ticket_price" to state.value.ticket.price,
-                    "ticket_quantity" to state.value.ticket.capacity,
-                    "main_organiser" to state.value.mainOrganiser,
-                    "organisers_list" to state.value.organiserList,
-                    "attendees_list" to state.value.attendeeList,
-                    "category" to state.value.category,
-                )
-            val newEvent = Event(eventHashMap)
-
             when (eventRepository.addEvent(newEvent)){
                 is Resource.Success ->{
+                    //update user!
+                    updateUserHostList(uid, newEvent.fireBaseID)
                     Log.d("CreateEventViewModel", "Successfully added event")}
                 is Resource.Failure ->{
                     Log.d("CreateEventViewModel", "Failed to add event")
@@ -81,18 +85,153 @@ constructor(
             }
         }
         }
+    private suspend fun updateUserHostList(uid : String, newEventId : String, state: MutableStateFlow<CreateEventUiState> = _uiState){
+        when (val userResource = userRepository.getUser(uid)){
+            is Resource.Success -> {
+                val user = userResource.data!!
+                val userValues =
+                    hashMapOf(
+                        "private/firstName" to user.firstName,
+                        "private/lastName" to user.lastName,
+                        "private/phoneNumber" to user.phoneNumber,
+                        "private/birthDate" to user.birthDate,
+                        "private/email" to user.email,
+                        "profilePicUrl" to user.profilePicUrl,
+                        "qrCodeUrl" to user.qrCodeUrl,
+                        "username" to user.username,
+                        "accountStatus" to user.accountStatus, //need a user bio!
+                        "eventsAttendeeList" to user.eventsAttendeeList,
+                        "eventsHostList" to user.eventsHostList.add(newEventId))
+                val newUser = User(userValues, uid)
+                userRepository.updateUser(newUser)
+            }
+            is Resource.Failure -> {
+                Log.d("CreateEventViewModel", "Failed to find user ${uid} in database")
+            }
+        }
+    }
+
+    fun validateFields(state: MutableStateFlow<CreateEventUiState>): Boolean {
+        state.value = state.value.copy(
+            eventNameIsError = state.value.eventName.isBlank(),
+            eventDescriptionIsError = state.value.eventDescription.isBlank(),
+            startDateIsError = !isValidDate(state.value.startDate),
+            endDateIsError = !isValidDate(state.value.endDate),
+            startTimeIsError = !isValidTime(state.value.startTime),
+            endTimeIsError = !isValidTime(state.value.endTime),
+            locationIsError = state.value.location.isBlank(),
+            ticketNameIsError = state.value.ticketName.isBlank(),
+            ticketCapacityIsError = !isValidNumber(state.value.ticketCapacity),
+            ticketPriceIsError = !isValidNumber(state.value.ticketPrice)
+        )
+
+        return !state.value.eventNameIsError &&
+                !state.value.eventDescriptionIsError &&
+                !state.value.startDateIsError &&
+                !state.value.endDateIsError &&
+                !state.value.startTimeIsError &&
+                !state.value.endTimeIsError &&
+                !state.value.locationIsError &&
+                !state.value.ticketNameIsError &&
+                !state.value.ticketCapacityIsError &&
+                !state.value.ticketPriceIsError
+    }
+
+    // Assuming simple validation functions exist:
+    private fun isValidDate(date: String): Boolean {
+        val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        format.isLenient = false
+        return try {
+            format.parse(date)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun isValidTime(time: String): Boolean {
+        val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+        format.isLenient = false
+        return try {
+            format.parse(time)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    private fun isValidNumber(number: String): Boolean {
+        val parsedNumber: Int? = number.toIntOrNull()
+        return (parsedNumber != null && parsedNumber > 0)
+    }
+
+    fun onEventNameChanged(eventName: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(eventName = eventName)
+    }
+
+    fun onEventPhotoUrlChanged(eventPhotoUrl: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(eventPhotoUrl = eventPhotoUrl)
+    }
+
+    fun onEventDescriptionChanged(eventDescription: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(eventDescription = eventDescription)
+    }
+
+    fun onStartDateChanged(startDate: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(startDate = startDate)
+    }
+
+    fun onEndDateChanged(endDate: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(endDate = endDate)
+    }
+
+    fun onStartTimeChanged(startTime: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(startTime = startTime)
+    }
+
+    fun onEndTimeChanged(endTime: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(endTime = endTime)
+    }
+
+    fun onLocationChanged(location: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(location = location)
+    }
+
+    fun onTicketNameChanged(ticketName: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(ticketName = ticketName)
+    }
+
+    fun onTicketCapacityChanged(ticketCapacity: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(ticketCapacity = ticketCapacity)
+    }
+
+    fun onTicketPriceChanged(ticketPrice: String, state: MutableStateFlow<CreateEventUiState>) {
+        state.value = state.value.copy(ticketPrice = ticketPrice)
+    }
 }
 //parse date to string, then add it to the firebase.
 data class CreateEventUiState(
-    var eventName: String = "",
-    var eventPhoto: String = "",
-    var start: LocalDateTime,
-    var end: LocalDateTime,
-    var location: Location,
-    var description: String,
-    var ticket: EventTicket,
-    var mainOrganiser: String,
-    val organiserList: MutableList<String>,
-    val attendeeList: MutableList<String>,
-    var category: EventCategory,
+    val eventName: String = "",
+    val eventPhotoUrl: String = "",
+    val eventDescription: String = "",
+    val eventCategory: EventCategory = EventCategory.COMMUNITY,
+    val startDate: String = "2025-01-01T05:00",
+    val endDate: String = "",
+    val startTime: String = "",
+    val endTime: String = "",
+    val location: String = "",
+    val ticketName : String = "",
+    val ticketCapacity : String = "",
+    val ticketPrice : String = "",
+    val organiserList: MutableList<String> = mutableListOf(),
+    val eventNameIsError: Boolean = false,
+    val eventDescriptionIsError: Boolean = false,
+    val startDateIsError: Boolean = false,
+    val endDateIsError: Boolean = false,
+    val startTimeIsError: Boolean = false,
+    val endTimeIsError: Boolean = false,
+    val locationIsError: Boolean = false,
+    val ticketNameIsError: Boolean = false,
+    val ticketCapacityIsError: Boolean = false,
+    val ticketPriceIsError: Boolean = false
 )
---
+++ app/src/test/java/com/github/se/eventradar/CreateEventViewModelUnitTest.kt
@@ -0,0 +1,135 @@
+package com.github.se.eventradar
+
+import android.util.Log
+import com.github.se.eventradar.model.Location
+import com.github.se.eventradar.model.User
+import com.github.se.eventradar.model.event.Event
+import com.github.se.eventradar.model.event.EventCategory
+import com.github.se.eventradar.model.event.EventTicket
+import com.github.se.eventradar.model.repository.event.IEventRepository
+import com.github.se.eventradar.model.repository.event.MockEventRepository
+import com.github.se.eventradar.model.repository.user.IUserRepository
+import com.github.se.eventradar.viewmodel.CreateEventUiState
+import com.github.se.eventradar.viewmodel.CreateEventViewModel
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+class CreateEventViewModelUnitTest {
+    private lateinit var viewModel: CreateEventViewModel
+    private lateinit var eventRepository: IEventRepository
+    private lateinit var userRepository: IUserRepository
+    private lateinit var mockUiState: MutableStateFlow<CreateEventUiState>
+
+    class MainDispatcherRule(
+        private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+    ) : TestWatcher() {
+        override fun starting(description: Description) {
+            Dispatchers.setMain(testDispatcher)
+        }
+
+        override fun finished(description: Description) {
+            Dispatchers.resetMain()
+        }
+    }
+
+    @get:Rule
+    val mainDispatcherRule = CreateEventViewModelUnitTest.MainDispatcherRule()
+
+    val test: MutableSet<String> = mutableSetOf("Test Organiser", "Organiser2")
+
+    val mockUser =
+        User(
+            userId = "1",
+            birthDate = "01/01/2000",
+            email = "test@test.com",
+            firstName = "John",
+            lastName = "Doe",
+            phoneNumber = "1234567890",
+            accountStatus = "active",
+            eventsAttendeeList = mutableListOf(),
+            eventsHostList = mutableListOf(),
+            friendsList = mutableListOf(),
+            profilePicUrl = "",
+            qrCodeUrl = "",
+            username = "john_doe")
+
+    private val mockEvent =
+        Event(
+            eventName = "Event 1",
+            eventPhoto = "",
+            start = LocalDateTime.now(),
+            end = LocalDateTime.now(),
+            location = Location(0.0, 0.0, "Test Location"),
+            description = "Test Description",
+            ticket = EventTicket("Test Ticket", 0.0, 1, 0),
+            mainOrganiser = "1",
+            organiserList = mutableListOf(),
+            attendeeList = mutableListOf("Test Attendee"),
+            category = EventCategory.COMMUNITY,
+            fireBaseID = "1")
+
+    @Before
+    fun setUp() {
+        eventRepository = MockEventRepository()
+        runBlocking {userRepository.addUser(mockUser)}
+        runBlocking{userRepository.updateUser(mockUser)}
+        viewModel = CreateEventViewModel(eventRepository, userRepository)
+    }
+    //failure cases.
+    @Test
+    fun testCreateEventSuccessful() = runTest {
+        mockkStatic(Log::class)
+        every { Log.d(any(), any()) } returns 0
+        val dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val timeFormat = DateTimeFormatter.ofPattern("HH:mm")
+        viewModel.onEventNameChanged(mockEvent.eventName, mockUiState)
+        viewModel.onEventDescriptionChanged(mockEvent.description, mockUiState)
+        viewModel.onStartDateChanged(mockEvent.start.format(dateFormat), mockUiState)
+        viewModel.onStartTimeChanged(mockEvent.start.format(timeFormat), mockUiState)
+        viewModel.onEndDateChanged(mockEvent.end.format(dateFormat), mockUiState)
+        viewModel.onEndTimeChanged(mockEvent.end.format(timeFormat), mockUiState)
+        viewModel.onLocationChanged(mockEvent.location.address, mockUiState)
+        viewModel.onTicketNameChanged(mockEvent.ticket.name, mockUiState)
+        viewModel.onTicketCapacityChanged(mockEvent.ticket.capacity.toString(), mockUiState)
+        viewModel.onTicketPriceChanged(mockEvent.ticket.price.toString(), mockUiState)
+
+        assert(viewModel.validateFields(mockUiState))
+        viewModel.addEvent()
+        coVerify { eventRepository.addEvent(any()) }
+        coVerify { userRepository.updateUser(any()) }
+
+        //check the eventRepository and userRepository
+        assert(viewModel.uiState.value.eventName == mockEvent.eventName)
+        assert(viewModel.uiState.value.eventDescription == mockEvent.description)
+        assert(viewModel.uiState.value.startDate == mockEvent.start.format(dateFormat))
+        assert(viewModel.uiState.value.startTime == mockEvent.start.format(timeFormat))
+        assert(viewModel.uiState.value.endDate == mockEvent.end.format(dateFormat))
+        assert(viewModel.uiState.value.endDate == mockEvent.end.format(timeFormat))
+        assert(viewModel.uiState.value.location == mockEvent.location.address)
+        assert(viewModel.uiState.value.ticketCapacity.toInt() == mockEvent.ticket.capacity)
+        assert(viewModel.uiState.value.ticketPrice.toDouble() == mockEvent.ticket.price)
+        assert(viewModel.uiState.value.ticketName == mockEvent.ticket.name)
+        assert(viewModel.uiState.value.eventCategory == mockEvent.category)
+
+        unmockkAll()
+    }
+}
