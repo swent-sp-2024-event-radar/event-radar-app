91d2416e23d5bceb54070a4ceca774c9952b0787
wisdomseeker2001 <jyotiigoel2001@gmail.com>
2024-05-28 19:19:36 +0200

    Update QrCodeScannerTicket.kt

--- app/src/main/java/com/github/se/eventradar/ui/qrCode/QrCodeScannerTicket.kt
+++ app/src/main/java/com/github/se/eventradar/ui/qrCode/QrCodeScannerTicket.kt
@@ -3,6 +3,7 @@ package com.github.se.eventradar.ui.qrCode
 import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
@@ -32,6 +33,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.se.eventradar.viewmodel.qrCode.QrCodeAnalyser
 import com.github.se.eventradar.viewmodel.qrCode.ScanTicketQrViewModel
+import java.util.concurrent.Executors
 
 @Composable
 fun QrCodeScannerTicket(viewModel: ScanTicketQrViewModel = hiltViewModel()) {
@@ -61,17 +63,17 @@ fun QrCodeScannerTicket(viewModel: ScanTicketQrViewModel = hiltViewModel()) {
     }
 
 // Shared variables for camera configuration
-    val preview = remember { Preview.Builder().build() }
-    val selector = remember {
-        CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
-    }
-    val imageAnalysis = remember {
-        ImageAnalysis.Builder()
-            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .build()
-    }
-
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+//    val preview = remember { Preview.Builder().build() }
+//    val selector = remember {
+//        CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+//    }
+//    val imageAnalysis = remember {
+//        ImageAnalysis.Builder()
+//            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+//            .build()
+//    }
+//
+//    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
 //    fun reinitializeCamera(previewView: PreviewView) {
 //        val cameraProvider = cameraFutureProvider.get()
@@ -103,26 +105,40 @@ fun QrCodeScannerTicket(viewModel: ScanTicketQrViewModel = hiltViewModel()) {
     Column(modifier = Modifier.fillMaxSize()) {
         if (hasCameraPermission) {
             Spacer(modifier = Modifier.height(80.dp))
-            AndroidView( // PreviewView !E for Composable hence need ot create AndroidView
+            AndroidView(
                 factory = { context ->
+                    val cameraExecutor = Executors.newSingleThreadExecutor()
                     val previewView = PreviewView(context)
-                    preview.setSurfaceProvider(previewView.surfaceProvider)
-                    imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(context), analyser)
-                    try {
-                        cameraFutureProvider
-                            .get()
-                            .bindToLifecycle(
-                                lifeCycleOwner, // only launched during current compose lifecycle
-                                selector,
-                                preview,
-                                imageAnalysis
+                    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+                    cameraProviderFuture.addListener({
+                        val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                        val preview =
+                            Preview.Builder().build().also { it.setSurfaceProvider(previewView.surfaceProvider) }
+                        val imageAnalyzer = ImageAnalysis.Builder()
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build()
+                            .also { it.setAnalyzer(cameraExecutor, analyser) }
+                        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                        try {
+                            // Unbind use cases before rebinding
+                            cameraProvider.unbindAll()
+
+                            // Bind use cases to camera
+                            cameraProvider.bindToLifecycle(
+                                context as ComponentActivity, cameraSelector, preview, imageAnalyzer
                             )
-                    } catch (e: Exception) {
-                        Log.e("QrCodeCameraFail", "Use case binding failed", e)
-                    }
+
+                        } catch (exc: Exception) {
+                            Log.e("DEBUG", "Use case binding failed", exc)
+                        }
+                    }, ContextCompat.getMainExecutor(context))
                     previewView
                 },
-                modifier = Modifier.weight(1.5f).aspectRatio(1f).padding(horizontal = 32.dp)
+                modifier = Modifier
+                    .weight(1.5f)
+                    .aspectRatio(1f)
+                    .padding(horizontal = 32.dp)
             )
 //            LaunchedEffect(key1 = uiState.action) {
 //                if (uiState.action == ScanTicketQrViewModel.Action.ScanTicket) {
